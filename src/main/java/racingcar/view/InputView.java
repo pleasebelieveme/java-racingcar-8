f19b8f8ee5b4adb83c0b3fb457eb28478672cfe1
believeme <cain1227@naver.com>
2025-10-27 16:40:36 +0900

    feat(view): InputView private 생성자 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -10,6 +10,10 @@ public class InputView {
 	private static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	private static final String DELIMITER = ",";
 
+	private InputView() {
+		throw new AssertionError("유틸리티 클래스는 인스턴스화할 수 없습니다.");
+	}
+
 	public static List<String> readCarNames() {
 		System.out.println(CAR_NAMES_INPUT_MESSAGE);
 		String input = Console.readLine();
