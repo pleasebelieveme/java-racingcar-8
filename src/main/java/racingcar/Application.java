79551388f54accb81ec7d0c8c357272acb2f4f88
believeme <cain1227@naver.com>
2025-10-27 21:52:26 +0900

    feat(app): Application 메인 실행 구현`

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,32 +3,16 @@ package racingcar;
 import java.util.List;
 
 import racingcar.domain.Car;
+import racingcar.service.RacingGameService;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
 		List<String> names = InputView.readCarNames();
-		System.out.println("입력된 이름: " + names);
-
 		int rounds = InputView.readRounds();
-		System.out.println("입력된 횟수: " + rounds);
 
-		Car car1 = new Car("pobi");
-		car1.move(true);
-		car1.move(true);
-
-		Car car2 = new Car("woni");
-		car2.move(true);
-
-		Car car3 = new Car("jun");
-		car3.move(true);
-		car3.move(true);
-		car3.move(true);
-
-		List<Car> cars = List.of(car1, car2, car3);
-
-		OutputView.printResultHeader();
-		OutputView.printRoundResult(cars);
+		RacingGameService racingGameService = new RacingGameService();
+		racingGameService.play(names, rounds);
     }
 }
