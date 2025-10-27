dc99e62b3545a43a7fbbbb48a4f9a60945bab090
believeme <cain1227@naver.com>
2025-10-27 21:30:28 +0900

    feat(view): 이동 상태 출력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,9 @@ package racingcar;
 
 import java.util.List;
 
+import racingcar.domain.Car;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
@@ -11,5 +13,22 @@ public class Application {
 
 		int rounds = InputView.readRounds();
 		System.out.println("입력된 횟수: " + rounds);
+
+		Car car1 = new Car("pobi");
+		car1.move(true);
+		car1.move(true);
+
+		Car car2 = new Car("woni");
+		car2.move(true);
+
+		Car car3 = new Car("jun");
+		car3.move(true);
+		car3.move(true);
+		car3.move(true);
+
+		List<Car> cars = List.of(car1, car2, car3);
+
+		OutputView.printResultHeader();
+		OutputView.printRoundResult(cars);
     }
 }
